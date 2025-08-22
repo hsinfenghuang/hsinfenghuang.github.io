86bb2c2cea4511f534d0c134e90f36271004ae9f
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-22 17:06:26 +0800

    20250822 v12

---
+++ hsinfenghuang.github.io.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\herokuhexohsinfenghuang\public"
+git init
+git add .
+git commit -m "20250822 v6"
+git remote add origin https://github.com/hsinfenghuang/hsinfenghuang.github.io.git
+git checkout -b main
+git push -u origin main --force
+pause
